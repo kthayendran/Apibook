e2d20771bcb61bebbd9cbab7b451124b4a0ee5ea
Thayendran <kobikessava@gmail.com>
2024-11-19 11:42:16 +0100

    dd

---
+++ Apibook/Entities/ApibookContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apibook.Entities;
+
+public partial class ApibookContext : DbContext
+{
+    public ApibookContext()
+    {
+    }
+
+    public ApibookContext(DbContextOptions<ApibookContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=;database=Apibook");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
